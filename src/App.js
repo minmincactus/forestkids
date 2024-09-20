49d8cb628670da2404c6a0970abc0414cd193872
minmincactus <115948035+minmincactus@users.noreply.github.com>
2024-09-20 03:04:10 -0400

    forest kids

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // v6 imports
+import Header from './Header';
+import Footer from './Footer';
+import Home from './Home';
+import Gallery from './Gallery';
+import Contact from './Contact';
+import './styles.css';
+import logo from './assets/logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+      <div className="logo-container">
+          <img src={logo} alt="Forest Kids Logo" className="logo-image" />
+        </div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
