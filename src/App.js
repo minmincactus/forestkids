578c36808bbe5b8d8121a07b35a17ca66ae27182
minmincactus <115948035+minmincactus@users.noreply.github.com>
2024-09-20 15:15:38 -0400

    restart

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // v6 imports
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import Home from './Home';
@@ -9,15 +9,14 @@ import logo from './assets/logo.png';
 
 function App() {
   return (
-    // Notice the `basename="/forestkids"` added to Router
-    <Router basename="/forestkids">
+    <Router basename="/forestkids"> {/* Set basename for GitHub Pages */}
       <div className="App">
         <div className="logo-container">
           <img src={logo} alt="Forest Kids Logo" className="logo-image" />
         </div>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} /> {/* This is the root path */}
+          <Route path="/" element={<Home />} />
           <Route path="/gallery" element={<Gallery />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
