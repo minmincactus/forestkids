74aa6a92c11150e4885eee8fdf36e02cb1229ae7
minmincactus <115948035+minmincactus@users.noreply.github.com>
2024-10-21 19:56:27 -0400

    added photos

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,42 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 const Header = () => {
+  const location = useLocation();
+
   return (
-    <header className="header">
-      <nav className="nav">
-        <ul className="nav-list">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/gallery">Gallery</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
-      </nav>
-    </header>
+    <nav className="header">
+      <ul className="nav-list">
+        <li>
+          <NavLink 
+            exact="true" 
+            to="/" 
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink 
+            to="/gallery"
+            className={({ isActive }) => 
+              (isActive || location.pathname.includes('/art')) ? "active" : ""
+            }
+          >
+            Gallery
+          </NavLink>
+        </li>
+        <li>
+          <NavLink 
+            to="/contact" 
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   );
-}
+};
 
 export default Header;
