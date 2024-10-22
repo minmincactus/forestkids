6649372b07ed56a0fbad913e5e51422864527856
minmincactus <115948035+minmincactus@users.noreply.github.com>
2024-10-21 23:16:48 -0400

    but back BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
-// import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import Home from './Home';
