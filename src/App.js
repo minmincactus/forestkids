b7ac19566e65731e3607df03cf7812ad789109df
minmincactus <115948035+minmincactus@users.noreply.github.com>
2024-10-21 23:19:36 -0400

    bro

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import Home from './Home';
@@ -10,10 +10,9 @@ import logo from './assets/logo.png';
 
 function App() {
   return (
-    <Router basename="/forestkids"> {/* Set basename for GitHub Pages */}
+    <Router> {/* Using HashRouter instead of BrowserRouter */}
       <div className="App">
         <div className="logo-container">
-          {/* Wrap the logo in a Link that points to the home page */}
           <Link to="/">
             <img src={logo} alt="Forest Kids Logo" className="logo-image" />
           </Link>
